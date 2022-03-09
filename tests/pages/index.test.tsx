2819829678a02bd06b562cc1ceb48521674ebf18
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-09 17:31:03 -0500

    Setup jest

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-// Using render and screen from test-utils.js instead of
-// @testing-library/react
 import { render, screen } from "../test-utils";
 import Home from "@pages/index";
 
 describe("HomePage", () => {
   it("should render the heading", () => {
     render(<Home />);
-
     const heading = screen.getByText(/Coming soon!/i);
-
-    // we can only use toBeInTheDocument because it was imported
-    // in the jest.setup.js and configured in jest.config.js
     expect(heading).toBeInTheDocument();
   });
 });
