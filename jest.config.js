e5f441eaa8643b72ff904de983e566539aaa8678
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-09 20:14:14 -0500

    Specs

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const jestConfig = {
   setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     "^@components(.*)$": "<rootDir>/components$1",
@@ -6,4 +6,11 @@ module.exports = {
     "^@hooks(.*)$": "<rootDir>/hooks$1",
   },
   testEnvironment: "jsdom",
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+  resetMocks: true,
 };
+
+module.exports = jestConfig;
