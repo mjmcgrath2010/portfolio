9c388172c75ecc74260ba05478e07b392e24527f
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-09 20:33:34 -0500

    Push

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "../test-utils";
-import Home from "@pages/index";
+import Home from "src/pages/index";
 
 describe("HomePage", () => {
   it("should render the heading", () => {
