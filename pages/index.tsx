e76cfb93df4e9179616b16531e8f5b13a98c4031
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-14 19:16:08 -0400

    Push

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { getPosts, mapQueriesToProps } from "@graphql-client/static";
 
-const Home: NextPage = ({ posts }: any) => {
+const Home: NextPage = () => {
   return (
     <div>
       <Head>
@@ -11,10 +11,10 @@ const Home: NextPage = ({ posts }: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Coming soon!</h1>
-      {JSON.stringify(posts)}
+      {/* {JSON.stringify(posts)} */}
     </div>
   );
 };
-export const getStaticProps = () => mapQueriesToProps([{ posts: getPosts }]);
+// export const getStaticProps = () => mapQueriesToProps([{ posts: getPosts }]);
 
 export default Home;
