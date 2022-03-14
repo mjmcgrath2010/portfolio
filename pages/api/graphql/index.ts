e302502932b7832638debed0ad711645e417e219
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-14 19:41:27 -0400

    Push

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -4,6 +4,7 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-co
 import type { PageConfig } from "next";
 import { buildSchema } from "type-graphql";
 import PostsResolver from "@graphql-server/resolvers/Posts";
+import { MongoClient } from "mongodb";
 
 // disable next js from handling this route
 export const config: PageConfig = {
@@ -12,11 +13,29 @@ export const config: PageConfig = {
   },
 };
 
+let db;
+
 const apolloServer = new ApolloServer({
   schema: await buildSchema({
     resolvers: [PostsResolver],
   }),
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  context: async () => {
+    if (!db) {
+      try {
+        const dbClient = new MongoClient(`${process.env.DB_URL}`, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+
+        await dbClient.connect();
+        db = dbClient.db("next-graphql"); // database name
+        return { db };
+      } catch (e) {
+        console.log("--->error while connecting with graphql context (db)", e);
+      }
+    }
+  },
 });
 
 await apolloServer.start();
