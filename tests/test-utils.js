0b10062d7f6694134d744652f7dc1622eea77461
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-24 18:28:40 -0400

    Session tests

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -3,11 +3,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import theme from "@components/theme";
+import { SessionProvider } from "next-auth/react";
 
 // Add in any providers here if necessary:
 // (ReduxProvider, ThemeProvider, etc)
 const Providers = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <SessionProvider session={{}}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </SessionProvider>
+  );
 };
 
 const customRender = (ui, options = {}) =>
