a10666c409dfa815188eb696cc1ca49d422d8848
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-24 06:27:02 -0400

    Fix sb

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -5,7 +5,7 @@ import NavBarProps, { LogoProps } from "./types";
 import AppBar from "../AppBar";
 import Logo from "../../public/assets/branding/logo.svg";
 import Link from "../Link";
-import useScroll from "@hooks/useScroll";
+import useScroll from "../../client/hooks/useScroll";
 
 interface RouteProps {
   href: string;
