c2e070fcd3ce2b9a0753667cfed6f7bdd93eb46b
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-25 07:17:07 -0400

    Signin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 import { getPosts, mapQueriesToProps } from "@gql/queries";
 
 const Home: NextPage = ({ posts }: any) => {
+  const { data: session } = useSession();
   return (
     <div>
       <Head>
@@ -11,7 +14,17 @@ const Home: NextPage = ({ posts }: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Coming soon!</h1>
-      {JSON.stringify(posts)}
+      {session ? (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
     </div>
   );
 };
