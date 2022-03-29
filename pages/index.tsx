965ffbc498f24f07c1d98663e5d5f135c7199dfb
Mike McGrath <mike@mikemcgrathonline.us>
2022-03-29 18:35:30 -0400

    Admin routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useSession, signIn, signOut } from "next-auth/react";
-
-import { AllPosts } from "@gql/queries";
-import { useQuery } from "@apollo/client";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  const { data, error, loading } = useQuery(AllPosts);
   return (
     <div>
       <Head>
@@ -15,18 +9,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Personal Portfolio and Blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Coming soon!</h1>
-      {session ? (
-        <>
-          Signed in as {session.user?.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      ) : (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      )}
+      <h1>Coming soon!</h1>]
     </div>
   );
 };
