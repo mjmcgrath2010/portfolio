c87d2e96cb3643b4adb272a5461706098a19e87d
Mike McGrath <mike@mikemcgrathonline.us>
2022-04-05 06:01:32 -0400

    add basic tests

--- tests/Input/index.test.tsx
+++ tests/Input/index.test.tsx
@@ -1,9 +1,13 @@
-import Input from '@components/Input';
+import Input from "@components/Input";
 
 import { render, screen } from "../test-utils";
 
-describe('<Input />', () => {
-  it('has no tests', () => {
-    expect(true).toBe(false);
+describe("<Input />", () => {
+  it("has no tests", () => {
+    const view = render(
+      <Input id="test" name="testField" onChange={jest.fn()} />
+    );
+
+    expect(view).toMatchSnapshot();
   });
 });
