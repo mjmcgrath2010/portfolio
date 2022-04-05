a76b49686b1e7b88f37d256193094af76e612dd3
Mike McGrath <mike@mikemcgrathonline.us>
2022-04-05 19:17:41 -0400

    WOrks

--- styles/global/index.ts
+++ styles/global/index.ts
@@ -7,7 +7,9 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     font-family: ${({ theme }) => theme.fontFamilies.sans_1};
     background:${({ theme }) => theme.colors.white_200};
-    color: ${({ theme }) => theme.palette.text.dark};
+    color: ${({ theme }) => theme.palette.text.light};
+    background: url("/assets/images/default-background.jpg") center;
+    background-position: cover;
  }
 `;
 
