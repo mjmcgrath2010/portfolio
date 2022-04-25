d9e0e4a43a94ec954806c552c4d48ad98d7a017a
Mike McGrath <mike@mikemcgrathonline.us>
2022-04-25 09:45:06 -0400

    Add types for global

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,5 +1,7 @@
 // global.d.ts
 import { mongoose } from "mongoose";
+import { MongoClient } from "mongodb";
 declare global {
   var mongoConnection: mongoose.Connection;
+  var _mongoClientPromise: Promise<MongoClient>;
 }
